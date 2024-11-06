42f790d4f3f65b104ec0d48ae80977d9af7dc516
bpnordin <bpnordin@gmail.com>
2024-11-06 14:45:41 -0700

    just made a nice graph

--- testing.py
+++ testing.py
@@ -11,13 +11,13 @@ routeNum = "1"
 
 radius = 0.005
 stop_coords = (40.769267, -111.882791)
-#y, x = stop_coords
+y, x = stop_coords
 
 df = db.getRouteLocationData(routeNum)
 df = db.calculateDistance(df)
 df = db.getInsidePoints(df)
 
-id = "24004"
+id = "24002"
 destination = "University Hospital"
 mask = (df['destination'] == destination) & (df['vehicle_id'] == id)
 df = df[mask].reset_index(drop=True)
@@ -31,17 +31,20 @@ split_dfs = []
 start_idx = 0
 for end_idx in gap_indices:
     #iloc is not inclusive, and end_idx is the place where the timestsamp is > 60
+    print(end_idx)
     split_dfs.append(df.iloc[start_idx:end_idx].copy())
     start_idx = end_idx 
 
 # Append the last part of the DataFrame
 split_dfs.append(df.iloc[start_idx:].copy())
-
+circle = Circle((x,y),radius=radius,color="red",fill=False)
+plt.gca().add_patch(circle)
 # Now, split_dfs is a list of DataFrames, each representing a continuous segment of data.
 for trip in split_dfs:
 
-    x = split_dfs[0]['longitude']
-    y = split_dfs[0]['latitude']
+    x = trip['longitude']
+    y = trip['latitude']
     plt.plot(x,y)
+    plt.scatter(trip[trip['inside']]['longitude'],trip[trip['inside']]['latitude'],color="red")
 
 plt.show()
